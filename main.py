f2a8c85da110b00c0f2956220151f45efa5fea91
antikhrom <takaev944@mail.ru>
2025-04-19 23:32:00 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-from app.bot import start_bot
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+
+# Вставь сюда свой токен бота
+TOKEN = "8193630010:AAEfYsNTX0T1qcdttCBTfP2b6aZcevSOvxI"
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+# Обработчик команды /start
+@dp.message_handler(commands=["start"])
+async def start_command(message: Message):
+    await message.answer("Привет! Я работаю!")
+
+async def main():
+    await dp.start_polling()
 
 if __name__ == "__main__":
-    start_bot()
+    asyncio.run(main())
