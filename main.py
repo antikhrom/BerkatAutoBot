3a2aae9902babd1043d532de7763e4ee5de678a5
antikhrom <takaev944@mail.ru>
2025-04-23 21:59:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
-import asyncio
-from aiogram import Bot, Dispatcher
-from aiogram.types import Message
+from aiogram import Bot, Dispatcher, types, executor
+import os
 
-# Вставь сюда свой токен бота
-TOKEN = "8193630010:AAEfYsNTX0T1qcdttCBTfP2b6aZcevSOvxI"
-
-bot = Bot(token=TOKEN)
+bot = Bot(token=os.getenv("BOT_TOKEN"))
 dp = Dispatcher(bot)
 
-# Обработчик команды /start
 @dp.message_handler(commands=["start"])
-async def start_command(message: Message):
+async def start(message: types.Message):
     await message.answer("Привет! Я работаю!")
 
-async def main():
-    await dp.start_polling()
-
 if __name__ == "__main__":
-    asyncio.run(main())
+    executor.start_polling(dp)
